a171943ab1e6306aacade09d4bf184adaf0d1244
jenbuska <joona.enbuska@eficode.com>
2017-03-31 16:39:15 +0300

    fix bugs on components exercise

--- src/components/ComponentPropsExercise.jsx
+++ src/components/ComponentPropsExercise.jsx
@@ -6,6 +6,11 @@ class ValidateInput extends React.Component {
   static propTypes = {
     validate: func.isRequired,
     placeholder: string.isRequired,
+    type: string.isRequired,
+  }
+
+  defaultProps = {
+    type: 'text',
   }
 
   state = { text: '', invalid: false }
@@ -36,11 +41,13 @@ class ValidateInput extends React.Component {
 
   render() {
     const { text, invalid } = this.state;
+    const { type } = this.props;
     return (
       <div className="validate-input-container">
         <input
+          type={type}
           placeholder={this.props.placeholder}
-          className={invalid ? 'input-default' : 'invalid-input'}
+          className={invalid ? 'invalid-input' : 'input-default'}
           onChange={this.onTextChanged}
           value={text}
           onBlur={this.onBlur}
@@ -56,13 +63,14 @@ class ValidateInput extends React.Component {
 export default class Component extends React.Component {
 
   state = { searching: false };
+  emailRegex = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/;
 
   render() {
     return (
       <div>
         <h1>{this.state.header}</h1>
-        <ValidateInput validate={text => text.length > 8} placeholder={'8 or more characters'} />
-
+        <ValidateInput validate={text => this.emailRegex.test(text)} type="email" placeholder={'email'} />
+        <ValidateInput validate={text => text.length > 8} type="password" placeholder={'8 or more characters'} />
       </div>
     );
   }
