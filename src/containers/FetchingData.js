5f6dd3e28545a499c2593f99e0cc4174840f1dac
jenbuska <joona.enbuska@eficode.com>
2017-04-02 23:41:43 +0300

    create fetcing data with axios example

--- src/containers/FetchingData.js
+++ src/containers/FetchingData.js
@@ -1,3 +1,29 @@
-/**
- * Created by joonaenbuska on 02/04/2017.
- */
+import React from 'react';
+import axios from '../axios';
+import TreeList from 'containers/TreeList';
+
+export default class FetchingData extends React.Component {
+
+  state = { status: 'idle', data: {}, };
+
+  sendRequest = async () => {
+    const { data, } = await axios.get('/',);
+    this.setState({ status: 'data fetched', data, });
+  };
+
+  fetchOptions = () => {
+    this.sendRequest();
+    this.setState({ status: 'fetching data', });
+  };
+
+  render() {
+    const { status, data, } = this.state;
+    return (
+      <div>
+        <h3>{status}</h3>
+        <button className="button-primary" onClick={this.fetchOptions}>Fetch api description</button>
+        <TreeList objectRoot={data} />
+      </div>
+    );
+  }
+}
