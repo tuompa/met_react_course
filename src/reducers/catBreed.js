abe600592585b090e5f1725384ce43fcebf52e5a
jenbuska <joona.enbuska@eficode.com>
2017-04-02 17:55:46 +0300

    remove unused files and rename others

---
+++ src/reducers/catBreed.js
@@ -0,0 +1,33 @@
+import { ADD_EMPLOYEE, REMOVE_EMPLOYEE, MODIFY_EMPLOYEE, } from '../actions/types';
+
+
+const initialState = {
+  1: { id: 1, name: 'Joona', },
+  2: { id: 2, name: 'Kalle', },
+};
+let nextId = 3;
+const { keys, } = Object;
+
+export default function employees(state = initialState, { type, payload, }) {
+  console.log(type);
+  switch (type) {
+  case ADD_EMPLOYEE:
+    /* expect payload to be a name
+    * Copy the current state and add a new employee with a new id*/
+    nextId++;
+    return { ...state, [nextId]: { id: nextId, name: payload, }, };
+  case MODIFY_EMPLOYEE:
+    /* expect payload to be object of shape {id: integer, name: string}
+    Take a shallow copy of state and overrider evaluated value of payload.id*/
+    return { ...state, [payload.id]: payload, };
+  case REMOVE_EMPLOYEE: {
+    /* expect payload to be id of existing employee
+    Take of shallow copy of state and filter out employee with id that matches payload */
+    return keys(state)
+      .filter(key => Number(key) !== payload)
+      .reduce((acc, key) => ({ ...acc, [key]: state[key], }), {/* initial state for acc*/});
+  }
+  default:
+    return state;
+  }
+}
