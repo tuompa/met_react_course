4bf4f178c47101722c3bca6c0eff7c97c4b6eca0
jenbuska <joona.enbuska@eficode.com>
2017-04-02 23:40:32 +0300

    extend sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,40 +1,42 @@
 import React from 'react';
-import { browserHistory } from 'react-router';
-import { SidebarItem, SidebarLink, SidebarPenLink, SidebarHomeItem } from './SidebarContent';
+import { browserHistory, } from 'react-router';
+import { SidebarItem, SidebarLink, SidebarPenLink, SidebarHomeItem, } from './SidebarContent';
 
-const LINKS = {
-  '/jsxSyntax': 'JSX Syntax',
-  '/Lists': 'JSX Lists',
-  '/componentState': 'Component State',
-  '/componentProps': 'Component Props',
-  '/react-redux': 'React Redux',
-};
+const LINKS = [
+  { path: '/jsxSyntax', name: 'JSX Syntax', exercise: true, },
+  { path: '/Lists', name: 'JSX Lists', exercise: true, },
+  { path: '/componentState', name: 'Component State', exercise: true, },
+  { path: '/componentProps', name: 'Component Props', exercise: true, },
+  { path: '/Redux', name: 'Redux', },
+  { path: '/react-redux', name: 'React Redux', exercise: true, },
+  { path: '/fetcing-data', name: 'Fetch Data', exercise: true, },
+];
 
 export default class Sidebar extends React.Component {
 
-  state = { pathname: null };
+  state = { pathname: null, };
   componentWillMount() {
-    this.setState({ pathname: browserHistory.getCurrentLocation().pathname });
-    this.routeSubscription = browserHistory.listen(({ pathname }) => this.setState({ pathname }));
+    this.setState({ pathname: browserHistory.getCurrentLocation().pathname, });
+    this.routeSubscription = browserHistory.listen(({ pathname, }) => this.setState({ pathname, }));
   }
 
   render() {
-    const { pathname } = this.state;
+    const { pathname, } = this.state;
     return (
       <section id="sidebar" className="sidebar" >
         <SidebarHomeItem selected={pathname === '/'} onClick={() => browserHistory.push('/')} id="homeLink" />
-        {Object.keys(LINKS).map(key => (
-          <SidebarItem key={key}>
+        {LINKS.map(({ path, name, exercise, }) => (
+          <SidebarItem key={path}>
             <SidebarLink
-              onClick={() => { browserHistory.push(key); }}
-              selected={key === pathname}
+              onClick={() => { browserHistory.push(path); }}
+              selected={path === pathname}
             >
-              {LINKS[key]}
+              {name}
             </SidebarLink>
-            <SidebarPenLink
-              selected={(`${key}/exercise`) === pathname}
-              onClick={() => { browserHistory.push(`${key}/exercise`); }}
-            />
+            {exercise && (<SidebarPenLink
+              selected={(`${path}/exercise`) === pathname}
+              onClick={() => { browserHistory.push(`${path}/exercise`); }}
+            />)}
           </SidebarItem>))}
       </section>
     );
