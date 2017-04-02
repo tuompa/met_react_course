109f9f91da7652997bf01c6a4ac42ba35ae23006
jenbuska <joona.enbuska@eficode.com>
2017-04-02 23:36:24 +0300

    refactor propsexercise

--- src/components/ComponentPropsExercise.jsx
+++ src/components/ComponentPropsExercise.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import catInvalid from '../images/cat-complete.png';
-const { func, string } = React.PropTypes;
+import catInvalid from '../images/cat-invalid.png';
+const { func, string, } = React.PropTypes;
 
 /* ValidateInput is a component that displays error
  * after focus has left on element is left if props validate
@@ -11,28 +11,28 @@ class ValidateInput extends React.Component {
     validate: func.isRequired,
     placeholder: string.isRequired,
     type: string.isRequired,
-  }
+  };
 
   defaultProps = {
     type: 'text',
-  }
+  };
 
-  state = { text: '', invalid: false }
+  state = { text: '', invalid: false, };
 
   onTextChanged = (event) => {
     const text = event.target.value;
-    const nextState = { text };
+    const nextState = { text, };
     /* change state.text */
     /* and validate text, if it is valid set state.invalid:false*/
-  }
+  };
 
   onBlur= () => {
     /* on blur validate text and set state invalid:true if it is not valid */
-  }
+  };
 
   render() {
-    const { text, invalid } = this.state;
-    const { type } = this.props;
+    const { text, invalid, } = this.state;
+    const { type, } = this.props;
     return (
       <div className="validate-input-container">
         <input
@@ -43,7 +43,11 @@ class ValidateInput extends React.Component {
           value={text}
           onBlur={this.onBlur}
         />
-        <img className={invalid ? 'invalid-indicator-visible' : 'invalid-indicator-hidden'} src={catInvalid} />
+        <img
+          role="presentation"
+          className={invalid ? 'invalid-indicator-visible' : 'invalid-indicator-hidden'}
+          src={catInvalid}
+        />
       </div>
     );
   }
@@ -52,13 +56,12 @@ class ValidateInput extends React.Component {
 }
 
 const emailRegex = /^(("[\w-\s]+")|([\w-]+(?:\.[\w-]+)*)|("[\w-\s]+")([\w-]+(?:\.[\w-]+)*))(@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$)|(@\[?((25[0-5]\.|2[0-4][0-9]\.|1[0-9]{2}\.|[0-9]{1,2}\.))((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){2}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\]?$)/;
-const form = () => (
+const Form = () => (
   <div>
-    <h1>{this.state.header}</h1>
     <ValidateInput validate={text => emailRegex.test(text)} type="email" placeholder="email" />
     <ValidateInput validate={text => text.length > 8} type="password" placeholder="password" />
   </div>
     );
 
-export default form;
+export default Form;
 
