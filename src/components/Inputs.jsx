7e92d6a48eb59efb294c4078b5903a8a1b189731
jenbuska <joona.enbuska@eficode.com>
2017-04-08 20:25:58 +0300

    create Inputs jsx

---
+++ src/components/Inputs.jsx
@@ -0,0 +1,21 @@
+
+import React from 'react';
+
+const {string,func,any,bool,} = React.PropTypes;
+const InputDefault = ({value,className='',placeholder,type='text',onChange = ()=>console.log('on change not implemented'),})=>(
+  <input
+    type={type}
+    value={value}
+    className={`input-default ${className}`}
+    placeholder={placeholder}
+    onChange={e=>onChange(e.target.value)}
+  />
+);
+InputDefault.propTypes = {
+  className: string,
+  placeholder: string,
+  type: string,
+  onChange: func,
+  value: any,
+};
+exports.InputDefault = InputDefault;
