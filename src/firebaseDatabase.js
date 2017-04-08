5ec6ef9994d5d253a9a07595ed9e512cd82747aa
jenbuska <joona.enbuska@eficode.com>
2017-04-08 20:22:27 +0300

    create firebase database provider

---
+++ src/firebaseDatabase.js
@@ -0,0 +1,30 @@
+import axios from './axios';
+import firebase from 'firebase';
+
+const {log,} = console;
+const config = {
+  apiKey: 'AIzaSyCtZbXDyJb_xhovQ9XcRKTTcO-MA8asBuU',
+  authDomain: 'reactcourse-83808.firebaseapp.com',
+  databaseURL: 'https://reactcourse-83808.firebaseio.com',
+  projectId: 'reactcourse-83808',
+  storageBucket: '',
+  messagingSenderId: '1054025612331',
+};
+firebase.initializeApp(config);
+const dbRef = firebase.database().ref();
+axios.get('/token').then(({data: token,})=>{
+  log({token,});
+  firebase.auth().signInWithCustomToken(token).catch((error)=>{
+    // Handle Errors here.
+    const {code,message,} = error;
+    log({code,message,});
+  });
+});
+
+export default dbRef;
+
+exports.signOut =()=>firebase.auth().signOut().then(()=>{
+  log('Signed Out');
+},(error)=>{
+  console.error('Sign Out Error',error);
+});
