33a2cac1412d07f90b8d43034a66221c228ec050
jenbuska <joona.enbuska@eficode.com>
2017-04-16 20:42:43 +0300

    refactor

--- src/components/TodoApp/TodoItem.jsx
+++ src/components/TodoApp/TodoItem.jsx
@@ -1,17 +1,14 @@
 import React from 'react';
 
-const {string, bool, func} = React.PropTypes;
-
-const TodoItem = ({description, done, onDoneToggle}) => {
-  return (<div className='todo-item'>
-    <div>{description}</div>
-    <input type='checkbox' checked={done} onChange={() => onDoneToggle(!done)} />
-  </div>)
-};
+const { string, bool, func, } = React.PropTypes;
+const TodoItem = ({ description, done, onDoneToggle, }) => (<div className='todo-item'>
+  <div>{description}</div>
+  <input type='checkbox' checked={done} onChange={() => onDoneToggle(!done)} />
+</div>);
 TodoItem.propTypes = {
   done: bool,
   description: string.isRequired,
-  onDoneToggle: func.isRequired
-}
+  onDoneToggle: func.isRequired,
+};
 
 export default TodoItem;
