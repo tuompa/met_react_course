69dc0ba6b0d62c58b5cffe2f9951ca9c65f4241d
jenbuska <joona.enbuska@eficode.com>
2017-04-30 15:17:02 +0300

    remove nonexisting status image

--- src/components/Lists.jsx
+++ src/components/Lists.jsx
@@ -5,14 +5,11 @@ function getRandomCatUrl() {
     100, 101, 200, 201, 202, 204, 206, 207, 300,
     301, 303, 304, 305, 307, 400, 401, 402, 403,
     404, 405, 406, 408, 409, 410, 411, 412, 413,
-    414, 405, 406, 407, 408, 420, 421, 422, 423,
-    423, 424, 425, 426, 429, 431, 444, 450, 401,
-    500, 502, 503, 504, 506, 507, 508, 509, 511,
-    599, ];
-  let index = parseInt(Math.random() * 10000) % STATUS_CODES.length;
-  if (!STATUS_CODES[index]) {
-    index++;
-  }
+    414, 420, 421, 422, 423, 423, 424, 425, 426,
+    429, 431, 444, 450, 401, 500, 502, 503, 504,
+    506, 507, 508, 509, 511, 599,
+  ];
+  const index = parseInt(Math.random() * 10000) % STATUS_CODES.length;
   return `https://http.cat/${STATUS_CODES[index]}`;
 }
 const CATS = [ getRandomCatUrl(), getRandomCatUrl(), getRandomCatUrl(), ];
