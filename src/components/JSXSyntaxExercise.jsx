e438bcbd15565453ab8772757ee9a35e35823cf0
Tuomas Paavola <tuomas.paavola@verco.fi>
2017-05-15 12:38:46 +0300

    exercise 1

--- src/components/JSXSyntaxExercise.jsx
+++ src/components/JSXSyntaxExercise.jsx
@@ -3,12 +3,17 @@ import React from 'react';
 const buttonText = 'Im a button';
 
 const Component = props => (
-  <div className='note-exercise-s'>
-    <div>
-      <h2>Make evaluated value of 'buttonText' be displayed in
-        a 'button' tag on the next line, and attach an onClick listener to it</h2>
+    <div className='note-exercise-s'>
+        <div>
+            <h2>Make evaluated value of 'buttonText' be displayed in
+                a 'button' tag on the next line, and attach an onClick listener to it</h2>
+            <button
+                onClick={event => {
+                    console.log('onClick!');
+                    console.log(event);
+                }} >{buttonText}</button>
+        </div>
     </div>
-  </div>
 );
 
 export default Component;
