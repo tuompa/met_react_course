33c411f29672923045eb6772e90b25ed5bfa312e
Tuomas Paavola <tuomas.paavola@verco.fi>
2017-05-15 13:18:48 +0300

    exercise 2

--- src/components/ListsExercise.jsx
+++ src/components/ListsExercise.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-
+const emojis = [ '😺', '😸', '😹', '😻', '😼', '😽', '🙀', '😿', '😾', ];
 const Component = props => (
   <div className='flex note-exercise-s'>
-    <div className='emoji'>{'😡'}</div>
-    <div className='emoji'>{'🙁'}</div>
-    <div className='emoji'>{'😐'}</div>
-    <div className='emoji'>{'🙂'}</div>
-    <div className='emoji'>{'😁'}</div>
-    {/* reimplement emojis using map -> emojis [...].map(...)*/}
+    <ul>
+      {emojis.map(i => <li className='emoji' key={i}>{i}</li>)}
+    </ul>
   </div>
 );
-/* 😺 😸 😹 😻 😼 😽 🙀 😿 😾*/
+/* <div className='emoji'>{'😡'}</div>
+ <div className='emoji'>{'🙁'}</div>
+ <div className='emoji'>{'😐'}</div>
+ <div className='emoji'>{'🙂'}</div>
+ <div className='emoji'>{'😁'}</div>
+ { reimplement emojis using map -> emojis [...].map(...)*/
+
 export default Component;
